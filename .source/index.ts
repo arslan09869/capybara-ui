0b5b50e0342410b6a622bf164b90cea706bee1df
Arslan09869 <arslan09869@gmail.com>
2025-12-16 20:13:17 +0530

    okka

--- .source/index.ts
+++ .source/index.ts
@@ -1,10 +1,10 @@
 // @ts-nocheck -- skip type checking
+import * as meta_0 from "../content/docs/meta.json?collection=meta&hash=1746254312000"
 import * as docs_4 from "../content/docs/components/input.mdx?collection=docs&hash=1746254312000"
 import * as docs_3 from "../content/docs/components/electricBorder.mdx?collection=docs&hash=1746254312000"
 import * as docs_2 from "../content/docs/components/button.mdx?collection=docs&hash=1746254312000"
 import * as docs_1 from "../content/docs/installation.mdx?collection=docs&hash=1746254312000"
 import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1746254312000"
-import * as meta_0 from "../content/docs/meta.json?collection=meta&hash=1746254312000"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
 export const docs = _runtime.doc<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"C:/Users/arshu/Desktop/react-ui-master/content/docs/index.mdx"}, data: docs_0 }, { info: {"path":"installation.mdx","absolutePath":"C:/Users/arshu/Desktop/react-ui-master/content/docs/installation.mdx"}, data: docs_1 }, { info: {"path":"components\\button.mdx","absolutePath":"C:/Users/arshu/Desktop/react-ui-master/content/docs/components/button.mdx"}, data: docs_2 }, { info: {"path":"components\\electricBorder.mdx","absolutePath":"C:/Users/arshu/Desktop/react-ui-master/content/docs/components/electricBorder.mdx"}, data: docs_3 }, { info: {"path":"components\\input.mdx","absolutePath":"C:/Users/arshu/Desktop/react-ui-master/content/docs/components/input.mdx"}, data: docs_4 }]);
